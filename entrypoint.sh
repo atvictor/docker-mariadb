2fadbb12289ca961de3efa62ac02428f127a69d6
Victor <atvictor@users.noreply.github.com>
2015-08-15 00:38:27 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  mysql_install_db
+  cat <<EOF | mysqld --bootstrap --loose-skip-ndbcluster
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user;
+CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db LIKE 'test%';
+EOF
+  chown -R mysql:mysql /var/lib/mysql
+fi
+
+exec "$@"
