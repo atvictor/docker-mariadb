d3fad3a66506cef213ba66bc51a2657ddb318a9d
Victor <atvictor@users.noreply.github.com>
2015-08-15 00:39:56 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
   cat <<EOF | mysqld --bootstrap --loose-skip-ndbcluster
 FLUSH PRIVILEGES;
 DELETE FROM mysql.user;
-CREATE USER 'root'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+CREATE USER 'root'@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';
 GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db LIKE 'test%';
