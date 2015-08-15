bd6ea3c9b9850c3f858f5dd9471d4a3ce2f26e05
Victor <atvictor@users.noreply.github.com>
2015-08-15 18:54:28 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ $(ls -ld /var/lib/mysql | awk '{print $3":"$4}') != "mysql:mysql" ]; then
+  chown -R mysql:mysql /var/lib/mysql
+fi
+
 if [ ! -d "/var/lib/mysql/mysql" ]; then
   mysql_install_db
   cat <<EOF | mysqld --bootstrap --loose-skip-ndbcluster
@@ -10,7 +14,6 @@ GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;
 DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db LIKE 'test%';
 EOF
-  chown -R mysql:mysql /var/lib/mysql
 fi
 
 exec "$@"
